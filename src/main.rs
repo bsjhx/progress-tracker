4016d737701b6161b44c4fafe1e42f99afa8e3e0
bsjhx <rakoczymichal@hotmail.com>
2024-02-17 23:06:38 +0100

    Added some put

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use rocket::serde::{Deserialize, json::Json};
+
+#[put("/challenge/update", data="<request>")]
+fn change_value_by(request: Json<ChangeValueByRequest>) -> Result<(), String> {
+    println!("{} - {}", request.user_id, request.change_value);
+    Ok(())
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct ChangeValueByRequest {
+    user_id: String,
+    change_value: i32
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![change_value_by])
 }
